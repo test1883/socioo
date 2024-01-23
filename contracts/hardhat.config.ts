7bbb3b56ca2459c7c3659dada031e16b30c08cb7
test1883 <parth.eng1210@gmail.com>
2024-01-22 22:09:57 -0800

    chore(hardhat): added fuji config

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,8 +1,22 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+require("dotenv").config();
+
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+    hardhat: {
+    },
+    fuji: {
+      url: "https://api.avax-test.network/ext/bc/C/rpc",
+      chainId: 43113,
+      accounts: {mnemonic: process.env.mnemonic}
+    },
+  }
 };
 
 export default config;
