c1513f1b93fdee52b69032534143d5f466450b2b
test1883 <parth.eng1210@gmail.com>
2024-01-25 01:10:32 -0800

    chore(kysely): added databases

--- worker/src/db/kysely.ts
+++ worker/src/db/kysely.ts
@@ -2,10 +2,13 @@ import { CamelCasePlugin, Kysely } from 'kysely'
 import { D1Dialect } from 'kysely-d1'
 
 import { Env } from '../env'
-import { NameInKysely } from '../models'
+import { CommentInKysely, NameInKysely, PostInKysely, ProfileInKysely } from '../models'
 
 export interface Database {
-  names: NameInKysely
+  names: NameInKysely,
+  profiles: ProfileInKysely,
+  posts: PostInKysely,
+  comments: CommentInKysely
 }
 
 export function createKysely(env: Env): Kysely<Database> {
