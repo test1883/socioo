425a11eb52e3f3ed3424f41d18493fd27179c39f
test1883 <parth.eng1210@gmail.com>
2024-01-24 22:46:03 -0800

    feat(kysely): created models

--- worker/src/models.ts
+++ worker/src/models.ts
@@ -9,6 +9,29 @@ export const ZodName = z.object({
   contenthash: z.string().optional(),
 })
 
+export const ZodProfile = z.object({
+  name: z.string().regex(/^[a-z0-9-.]+$/),
+  followers: z.record(z.string()).optional(),
+  following: z.record(z.string()).optional(),
+  current_challenge: z.string().optional()
+})
+
+export const ZodPost = z.object({
+  name: z.string().regex(/^[a-z0-9-.]+$/),
+  post_id: z.number(),
+  likes: z.record(z.string()).optional(),
+  challenge: z.string().optional(),
+  files: z.record(z.string()),
+})
+
+export const ZodComment = z.object({
+  owner: z.string().regex(/^[a-z0-9-.]+$/),
+  name: z.string().regex(/^[a-z0-9-.]+$/),
+  post_id: z.number(),
+  comment: z.string(),
+  comment_id: z.number(),
+})
+
 export const ZodNameWithSignature = ZodName.extend({
   signature: z.object({
     hash: z.string(),
@@ -16,8 +39,36 @@ export const ZodNameWithSignature = ZodName.extend({
   }),
 })
 
+export const ZodProfileWithSignature = ZodProfile.extend({
+  signature: z.object({
+    hash: z.string(),
+    message: z.string(),
+  }),
+})
+
+export const ZodPostWithSignature = ZodPost.extend({
+  signature: z.object({
+    hash: z.string(),
+    message: z.string(),
+  }),
+})
+
+export const ZodCommentWithSignature = ZodComment.extend({
+  signature: z.object({
+    hash: z.string(),
+    message: z.string(),
+  }),
+})
+
 export type Name = z.infer<typeof ZodName>
+export type Profile = z.infer<typeof ZodProfile>
+export type Post = z.infer<typeof ZodPost>
+export type Comment = z.infer<typeof ZodComment>
+
 export type NameWithSignature = z.infer<typeof ZodNameWithSignature>
+export type ProfileWithSignature = z.infer<typeof ZodProfileWithSignature>
+export type PostWithSignature = z.infer<typeof ZodPostWithSignature>
+export type CommentWithSignature = z.infer<typeof ZodCommentWithSignature>
 
 export interface NameInKysely {
   name: string
@@ -25,6 +76,33 @@ export interface NameInKysely {
   addresses: string | null // D1 doesn't support JSON yet, we'll have to parse it manually
   texts: string | null // D1 doesn't support JSON yet, we'll have to parse it manually
   contenthash: string | null
+  createdAt: ColumnType<Date, never, never>
+  updatedAt: ColumnType<Date, never, string | undefined>
+}
+
+export interface ProfileInKysely {
+  name: string,
+  followers: string | null,
+  following: string | null,
+  currentChallenge: string | null
+}
+
+export interface PostInKysely {
+  name: string,
+  postId: number,
+  likes: string | null,
+  challenge: string | null,
+  files: string,
+  createdAt: ColumnType<Date, never, never>
+  updatedAt: ColumnType<Date, never, string | undefined>
+}
+
+export interface CommentInKysely {
+  owner: string,
+  name: string,
+  postId: number,
+  comment: string,
+  commentId: string,
   createdAt: ColumnType<Date, never, never>
   updatedAt: ColumnType<Date, never, string | undefined>
 }
