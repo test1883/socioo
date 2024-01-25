7f70a441f7adc8ceaad37001697531b19cb3f9c6
test1883 <parth.eng1210@gmail.com>
2024-01-25 02:01:45 -0800

    fix(worker): added name constraint

--- worker/src/handlers/functions/posts/get.ts
+++ worker/src/handlers/functions/posts/get.ts
@@ -3,12 +3,13 @@ import { Env } from '../../../env'
 import { Post } from '../../../models'
 import { parsePostFromDb } from './utils'
 
-export async function get(postId: number, env: Env): Promise<Post | null> {
+export async function get(name: string, postId: number, env: Env): Promise<Post | null> {
   const db = createKysely(env)
   const record = await db
     .selectFrom('posts')
     .selectAll()
     .where('postId', '=', postId)
+    .where('name', '=', name)
     .executeTakeFirst()
 
   if (!record) {
