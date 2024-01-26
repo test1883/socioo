54f044096c57b2b22a795c58c79171d37ce944f6
test1883 <parth.eng1210@gmail.com>
2024-01-26 01:19:19 -0800

    feat(worker): added profile routes

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,17 +1,21 @@
 import { Router, createCors } from 'itty-router'
 
 import { Env } from './env'
-import { getCcipRead, getName, getNames, setName } from './handlers/names'
+import * as names from './handlers/names'
+import * as profiles from './handlers/profiles'
 
 const { preflight, corsify } = createCors()
 const router = Router()
 
 router
   .all('*', preflight)
-  .get('/lookup/*', (request, env) => getCcipRead(request, env))
-  .get('/get/:name', (request, env) => getName(request, env))
-  .get('/names', (request, env) => getNames(env))
-  .post('/set', (request, env) => setName(request, env))
+  .get('/lookup/*', (request, env) => names.getCcipRead(request, env))
+  .get('/get-name/:name', (request, env) => names.getName(request, env))
+  .get('/names', (request, env) => names.getNames(env))
+  .post('/set-name', (request, env) => names.setName(request, env))
+  .get('/get-profile/:name', (request, env) => profiles.getProfile(request, env))
+  .get('/profiles', (request, env) => profiles.getProfiles(env))
+  .post('/set-profile', (request, env) => profiles.setProfile(request, env))
   .all('*', () => new Response('Not found', { status: 404 }))
 
 // Handle requests to the Worker
