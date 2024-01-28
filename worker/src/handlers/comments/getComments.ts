ddb5af7c6768d45dd2205aa526ef96bc09b3042c
test1883 <parth.eng1210@gmail.com>
2024-01-28 00:38:43 -0800

    refactor(comments): added zod safeParse

--- worker/src/handlers/comments/getComments.ts
+++ worker/src/handlers/comments/getComments.ts
@@ -1,8 +1,24 @@
+import { IRequest } from 'itty-router'
+import zod from 'zod'
+
 import { createKysely } from '../../db/kysely'
 import { Env } from '../../env'
 import { parseCommentFromDb } from '../functions/comments/utils'
 
-export async function getComments(postId: number ,env: Env) {
+export async function getComments(request: IRequest ,env: Env) {
+
+  const schema = zod.object({
+    postId: zod.number(),
+  })
+  const safeParse = schema.safeParse(request.params)
+
+  if (!safeParse.success) {
+    const response = { error: safeParse.error }
+    return Response.json(response, { status: 400 })
+  }
+
+  const { postId } = safeParse.data
+
   const db = createKysely(env)
   const comments = await db.selectFrom('comments').selectAll().where('postId', '=', postId).execute()
   const parsedComments = parseCommentFromDb(comments)
