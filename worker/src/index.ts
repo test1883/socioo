d21894707bd62027bbcc69d3a6bf2ec4179cbb99
test1883 <parth.eng1210@gmail.com>
2024-01-28 00:39:11 -0800

    feat(worker): added comment routes

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -3,6 +3,7 @@ import { Router, createCors } from 'itty-router'
 import { Env } from './env'
 import * as names from './handlers/names'
 import * as profiles from './handlers/profiles'
+import * as comments from './handlers/comments'
 
 const { preflight, corsify } = createCors()
 const router = Router()
@@ -16,6 +17,8 @@ router
   .get('/get-profile/:name', (request, env) => profiles.getProfile(request, env))
   .get('/profiles', (request, env) => profiles.getProfiles(env))
   .post('/set-profile', (request, env) => profiles.setProfile(request, env))
+  .get('/comments/:postId', (request, env) => comments.getComments(request, env))
+  .post('/set-comment', (request, env) => comments.setComment(request, env))
   .all('*', () => new Response('Not found', { status: 404 }))
 
 // Handle requests to the Worker
